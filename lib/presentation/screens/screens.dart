c8ad3bb3e9fb3235ac499a1ce5e234775effc85c
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-25 10:03:47 +0100

    add screens

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,5 +1,11 @@
 
-
+export 'package:flutter/src/widgets/framework.dart';
+export 'package:flutter/src/widgets/placeholder.dart';
+export 'package:widget_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widget_app/presentation/screens/app_tutorial/app_tutorial.dart';
+export 'package:widget_app/presentation/screens/infinite_scroll/infinite_scroll_creen.dart';
+export 'package:widget_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
