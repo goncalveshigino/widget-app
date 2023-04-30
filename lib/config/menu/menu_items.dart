6f1985121eacb640c8e882b7f4904b54e83b9542
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-30 09:29:15 +0100

    animated container finished

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -46,4 +46,11 @@ const appMenuItems = <MenuItem> [
     icon: Icons.info_outline,
   ),
 
+   MenuItem(
+    title: 'Animated Container', 
+    subtitle: 'Animated', 
+    link: '/animated', 
+    icon: Icons.check_box_outline_blank_outlined,
+  ),
+
 ];
